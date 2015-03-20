35c59c2a04d3133dd0d922e19cf6f8aa382700af
gamekid88 <gamekid88@hotmail.com>
2015-03-20 09:49:43 -0400

    Created a new file
    This new file will be my main settings page where most of my functions
    will be located.

--- blp_admin.php
+++ blp_admin.php
@@ -4,7 +4,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 * Plugin Name: Eric's Book Logging System
 * Description: This plugin will allows the admin to enter, edit, and delete book information. This information will be entered post meta and will require
  * no database. Shortcodes are included that will allow the admin to show all information, show author information, and allow a widget with book names to be 
- * created.
+ * created. Also inserting, editing, and deleting functionality is included.
 * Version: 1.0
 * Author: Eric Rathmann
 * Author URI: http://www.mylocalwebstop.com/
@@ -35,17 +35,33 @@ function book_panel()
 
 function custom_post ()
 {
+    $labels = array(
+		'name'               => 'Books',
+		'singular_name'      => 'Book',
+		'menu_name'          => 'Books',
+		'name_admin_bar'     => 'Books',
+		'add_new'            => 'Add New Book',
+		'add_new_item'       => 'Add New Book',
+		'new_item'           =>  'New Book',
+		'edit_item'          =>  'Edit Book',
+		'view_item'          =>  'View Book',
+		'all_items'          => 'All Books',
+		'search_items'       => 'Search Books',
+		'parent_item_colon'  =>  'Parent Books:',
+		'not_found'          => 'No books found.',
+		'not_found_in_trash' =>  'No books found in Trash.'
+	); 
+        
+        
     $args = array(
+      'labels' => $labels,
       'public' => true,
       'label'  => 'Books',
       'post_title' => 'book_title'
     );
-    register_post_type( 'book', $args );
-
+    register_post_type( 'book', $args );   
     
     
-    
-    
-}
+}// end custom post
 
 ?>
---
+++ blp_mainsettings.php
@@ -0,0 +1,15 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit;
+/**
+ * @package     BLP
+ * @copyright   Copyright (c) 2014, Eric Rathmann
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       1.0
+ */
+
+
+
+
+
+
+?>
